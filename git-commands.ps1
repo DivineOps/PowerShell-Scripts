695d5d2dab13ef0794da9f96de05093094a6b0ec
Sasha Rosenbaum <sasrose@microsoft.com>
2016-01-13 14:25:39 -0600

    First commit

--- git-commands.ps1
+++ git-commands.ps1
@@ -1,3 +1,11 @@
 #clone existing repo
 git clone https://github.com/DivineOps/PowerShell-Scripts.git
 
+#add all new file changes in current folder
+git add .
+
+#local commit
+git commit -m "My commit message"
+
+#push to remote repo
+git push origin master
